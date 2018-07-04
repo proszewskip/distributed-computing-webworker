1cbe5588d01d024f0b522dd22d01908e93ed93db
proszewskip <przemek.proszewski@gmail.com>
2018-07-04 17:43:01 +0200

    add sample project

---
+++ FactorialTask/FactorialSubtask.cs
@@ -0,0 +1,26 @@
+﻿using DistributedComputing.Common;
+
+namespace DistributedComputing.FactorialTask
+{
+    public class FactorialSubtask : ISubtask
+    {
+        public string Perform(string input)
+        {
+            var x = int.Parse(input);
+            var result = Factorial(x);
+
+            return result.ToString();
+        }
+
+        private int Factorial(int n)
+        {
+            if (n < 0) return 0;
+
+            var result = 1;
+
+            for (var i = 1; i <= n; i++) result *= i;
+
+            return result;
+        }
+    }
+}
