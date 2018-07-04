37ad35951b09b01f2a9b82c630c3a9e35b190c1d
proszewskip <przemek.proszewski@gmail.com>
2018-07-04 17:51:17 +0200

    add server project

---
+++ Server/Services/SubtaskFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using DistributedComputing.Common;
+
+namespace Server.Services
+{
+    internal class SubtaskFactory : ISubtaskFactory
+    {
+        public List<string> SubtasksInputs { get; } = new List<string>();
+
+        public void CreateNewSubtask(string input)
+        {
+            // This method should queue instantiating a new subtask on some distributed node.
+            Console.WriteLine("Creating a new subtask with data: " + input);
+            SubtasksInputs.Add(input);
+        }
+    }
+}
