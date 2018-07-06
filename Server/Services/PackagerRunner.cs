2ba11832767181566eb76ec19217ee0ab6f9a32d
proszewskip <przemek.proszewski@gmail.com>
2018-07-06 16:23:10 +0200

    [server] extract commandRunner

--- Server/Services/PackagerRunner.cs
+++ Server/Services/PackagerRunner.cs
@@ -1,5 +1,6 @@
-using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
+using Server.Services.CommandRunner;
 
 namespace Server.Services
 {
@@ -9,49 +10,31 @@ namespace Server.Services
         private readonly string _monoPackagerPath;
         private readonly string _outputDirectory;
 
-        public PackagerRunner(string monoPackagerPath, string inputDirectory, string outputDirectory)
+        private readonly ICommandRunner _commandRunner;
+
+        private const string MonoCommand = "mono";
+
+        public PackagerRunner(string monoPackagerPath, string inputDirectory, string outputDirectory, ICommandRunner commandRunner)
         {
             _inputDirectory = inputDirectory;
             _outputDirectory = outputDirectory;
             _monoPackagerPath = monoPackagerPath;
+
+            _commandRunner = commandRunner;
         }
 
-        public async Task<int> PackAssemblyAsync(string assemblyDirectory, string assemblyName)
+        public async Task<int> PackAssemblyAsync(string assemblyDirectoryName, string assemblyName)
         {
-            var packagerOutputPath = $"{_outputDirectory}/{assemblyDirectory}";
-            var prefixArg = $"-prefix={_inputDirectory}/{assemblyDirectory}";
-            var outArg = $"-out={packagerOutputPath}";
-            var packCommand = $"{_monoPackagerPath} {prefixArg} {outArg} {assemblyName}";
+            var packagerOutputPath = Path.Combine(_outputDirectory, assemblyDirectoryName);
+            var assembliesDirectoryPath = Path.Combine(_inputDirectory, assemblyDirectoryName);
 
-            var exitCode = await RunCommandTask(packCommand);
+            var prefixArg = $"-prefix={assembliesDirectoryPath}";
+            var outArg = $"-out={packagerOutputPath}";
+            var packCommandArgs = $"{_monoPackagerPath} {prefixArg} {outArg} {assemblyName}";
+
+            var exitCode = await _commandRunner.RunCommandTask(MonoCommand, packCommandArgs);
 
             return exitCode;
-        }
-
-        private Task<int> RunCommandTask(string cmdArgs)
-        {
-            var taskCompletionSource = new TaskCompletionSource<int>();
-
-            var packagerProcess = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "mono",
-                    Arguments = cmdArgs,
-                    CreateNoWindow = true
-                },
-                EnableRaisingEvents = true
-            };
-
-            packagerProcess.Exited += (sender, args) =>
-            {
-                taskCompletionSource.SetResult(packagerProcess.ExitCode);
-                packagerProcess.Dispose();
-            };
-
-            packagerProcess.Start();
-
-            return taskCompletionSource.Task;
         }
     }
 }
