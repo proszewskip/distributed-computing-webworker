b0425b15c8f1814f3737bb351c366d9d8bcfc030
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-07-07 20:26:13 +0200

    [server] add saving and compiling task definitions to wasm

--- Server/Controllers/DistributedTaskDefinitionController.cs
+++ Server/Controllers/DistributedTaskDefinitionController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Server.DTO;
 using Server.Models;
 using Server.Pagination;
+using Server.Services;
+using Server.Services.AssemblyAnalyzer;
+using Server.Services.PathsProvider;
 using Server.Validation;
 
 namespace Server.Controllers
@@ -18,16 +23,23 @@ namespace Server.Controllers
     public class DistributedTaskDefinitionController : ControllerBase
     {
         private readonly DistributedComputingDbContext _dbContext;
-        private readonly IConfiguration _configuration;
+        private readonly IPathsProvider _pathsProvider;
+        private readonly IAssemblyAnalyzer _assemblyAnalyzer;
+        // TODO: use IPackagerRunner instead of PackagerRunner
+        private readonly PackagerRunner _packagerRunner;
 
 
         public DistributedTaskDefinitionController(
             DistributedComputingDbContext dbContext,
-            IConfiguration configuration
+            IPathsProvider pathsProvider,
+            IAssemblyAnalyzer assemblyAnalyzer,
+            PackagerRunner packagerRunner
         )
         {
             _dbContext = dbContext;
-            _configuration = configuration;
+            _pathsProvider = pathsProvider;
+            _assemblyAnalyzer = assemblyAnalyzer;
+            _packagerRunner = packagerRunner;
         }
 
         [HttpGet]
@@ -54,45 +66,72 @@ namespace Server.Controllers
 
         [HttpPost]
         [ValidateModelAttribute]
-        public IActionResult Create([FromForm] CreateDistributedTaskDefinitionDTO body)
+        public async Task<IActionResult> Create([FromForm] CreateDistributedTaskDefinitionDTO body)
         {
+            // FIXME: handle errors
+
             // 1. Validate input data
             var taskDefinitionExists = _dbContext.DistributedTaskDefinitions
                 .Any(taskDefinition => taskDefinition.Name == body.Name);
 
             if (taskDefinitionExists)
             {
-                ModelState.TryAddModelError(nameof(body.Name), $"A task definition with name {body.Name} already exists");
+                ModelState.TryAddModelError(
+                    nameof(body.Name),
+                    $"A task definition with name {body.Name} already exists"
+                );
 
                 return new ValidationFailedResult(ModelState);
             }
 
             // 2. Analyze the DLL
-            // TODO: analyze the DLL
+            var taskDefinitionGuid = Guid.NewGuid();
+            var taskDefinitionDirectoryPath = Path.Join(
+                _pathsProvider.TaskDefinitionsDirectoryPath,
+                taskDefinitionGuid.ToString()
+            );
 
-            // 3. Add the data to the database
+            Directory.CreateDirectory(taskDefinitionDirectoryPath);
 
-            // FIXME: get `subtaskInfo` from DLL analysis
-            var subtaskInfo = new SubtaskInfo()
+            // REFACTOR: extract saving the DLLs
+            var mainDllPath = Path.Join(taskDefinitionDirectoryPath, body.MainDll.FileName);
+            using (var mainDllFileStream = new FileStream(mainDllPath, FileMode.Create))
             {
-                AssemblyName = "FactorialTask",
-                Namespace = "FactorialTask",
-                ClassName = "FactorialSubtask"
-            };
+                await body.MainDll.CopyToAsync(mainDllFileStream);
+            }
 
+            foreach (var additionalDllFile in body.AdditionalDlls)
+            {
+                var additionalDllPath = Path.Join(taskDefinitionDirectoryPath, additionalDllFile.FileName);
+                using (var additionalDllFileStream = new FileStream(additionalDllPath, FileMode.Create))
+                {
+                    await additionalDllFile.CopyToAsync(additionalDllFileStream);
+                }
+            }
+
+            var mainDllAssembly = Assembly.LoadFrom(mainDllPath);
+            var subtaskInfo = _assemblyAnalyzer.GetSubtaskInfo(mainDllAssembly);
+
+            // 3. Run packager
+            // TODO: display the result of packager
+            await _packagerRunner.PackAssemblyAsync(taskDefinitionGuid.ToString(), body.MainDll.FileName);
+
+            // 4. Add the data to the database
             var distributedTaskDefinition = new DistributedTaskDefinition()
             {
                 Name = body.Name,
-                OutputDirectory = "...",
+                OutputDirectory = Path.Combine(
+                    _pathsProvider.CompiledTasksDefinitionsDirectoryPath,
+                    taskDefinitionGuid.ToString(),
+                    body.MainDll.FileName
+                ),
                 SubtaskInfo = subtaskInfo
             };
 
             _dbContext.DistributedTaskDefinitions.Add(distributedTaskDefinition);
             _dbContext.SaveChanges();
 
-            // TODO: move the DLL to an output directory
-
-            // 4. Return the info back to the user
+            // 5. Return the info back to the user
 
             return Ok(distributedTaskDefinition);
         }
