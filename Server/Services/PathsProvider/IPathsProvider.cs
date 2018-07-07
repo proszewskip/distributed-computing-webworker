a84e4f8b6566284f81a4062449df39eb403a0860
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-07-07 20:23:59 +0200

    [server] add PathsProvider

---
+++ Server/Services/PathsProvider/IPathsProvider.cs
@@ -0,0 +1,14 @@
+namespace Server.Services.PathsProvider
+{
+    public interface IPathsProvider
+    {
+        string MonoPackagerPath { get; }
+
+        /// Path to the directory that contains subdirectories with DLLs for the task definition
+        string TaskDefinitionsDirectoryPath { get; }
+
+        /// Path to the directory that contains subdirectories with WASM packaged code ready for
+        /// clients to consume
+        string CompiledTasksDefinitionsDirectoryPath { get; }
+    }
+}
