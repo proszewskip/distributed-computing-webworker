8becb332005ec3e24f1d4154b2609e4f7e941740
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-07-09 18:08:37 +0200

    [server] handle task descriptions

--- Server/DTO/CreateDistributedTaskDTO.cs
+++ Server/DTO/CreateDistributedTaskDTO.cs
@@ -8,6 +8,8 @@ namespace Server.DTO
         [StringLength(255, MinimumLength = 3)]
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         [Required] public int Priority { get; set; }
 
         [Required] public int DistributedTaskDefinitionId { get; set; }
