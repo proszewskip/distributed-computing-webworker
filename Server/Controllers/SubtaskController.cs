984c247feb95b6c9ff2c25493d62cea8aebdce90
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-08-04 13:04:01 +0200

    [server] remove creating subtasks endpoint, refactor restarting subtasks

--- Server/Controllers/SubtaskController.cs
+++ Server/Controllers/SubtaskController.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Server.DTO;
 using Server.Models;
 using Server.Pagination;
-using Server.Services;
 using Server.Validation;
 
 namespace Server.Controllers
@@ -16,16 +14,13 @@ namespace Server.Controllers
     public class SubtaskController : Controller
     {
         private readonly DistributedComputingDbContext _dbContext;
-        private readonly ISubtaskFactoryFactory _subtaskFactoryFactory;
 
 
         public SubtaskController(
-            DistributedComputingDbContext dbContext,
-            ISubtaskFactoryFactory subtaskFactoryFactory
+            DistributedComputingDbContext dbContext
         )
         {
             _dbContext = dbContext;
-            _subtaskFactoryFactory = subtaskFactoryFactory;
         }
 
         [HttpGet]
@@ -47,52 +42,10 @@ namespace Server.Controllers
             return Ok(foundSubtask);
         }
 
-        [HttpPost]
+        [HttpPost("{id}/restart")]
         [ValidateModel]
-        public IActionResult Create([FromBody] CreateSubtaskDTO body)
+        public IActionResult Restart(int id)
         {
-            // FIXME: handle errors
-
-            var distributedTask = _dbContext.DistributedTasks
-                .FirstOrDefault(task => task.Id == body.DistributedTaskId);
-
-            if (distributedTask == null)
-            {
-                ModelState.TryAddModelError(
-                    nameof(body.DistributedTaskId),
-                    $"A distributed task with id{body.DistributedTaskId} does not exist."
-                );
-
-                return new ValidationFailedResult(ModelState);
-            }
-
-            var subtaskFactory = _subtaskFactoryFactory.CreateSubtaskFactory(distributedTask.Id);
-
-            try
-            {
-                subtaskFactory.CreateNewSubtask(body.InputData);
-            }
-            catch
-            {
-                // TODO: handle errors resulting from defining tasks
-            }
-
-            var createdSubtask = _dbContext.Subtasks
-                .Where(subtask => subtask.DistributedTaskId == body.DistributedTaskId &&
-                                  subtask.SequenceNumber == body.SequenceNumber)
-                .OrderByDescending(subtask => subtask.Id).First();
-
-            _dbContext.SaveChanges();
-
-            return CreatedAtAction(nameof(GetById), new { id = createdSubtask.Id }, createdSubtask);
-        }
-
-        [HttpPut("{id}")]
-        [ValidateModel]
-        public IActionResult Put(int id)
-        {
-            // 1. Validate input data
-
             var restartedSubtask = _dbContext.Subtasks.FirstOrDefault(subtask => subtask.Id == id);
 
             if (restartedSubtask == null)
@@ -106,7 +59,7 @@ namespace Server.Controllers
             }
 
             restartedSubtask.Status = SubtaskStatus.WaitingForExecution;
-            restartedSubtask.Result = "";
+            restartedSubtask.Result = null;
             restartedSubtask.Token = Guid.NewGuid().ToString();
 
             _dbContext.SaveChanges();
