aae6bc311a2c60fb505b72adb4e9740b1c4ec834
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-08-05 13:17:10 +0200

    [server] add Required annotations in models

--- Server/Models/DistributedTask.cs
+++ Server/Models/DistributedTask.cs
@@ -1,34 +1,39 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using JsonApiDotNetCore.Models;
 
 namespace Server.Models
 {
     public class DistributedTask : Identifiable
     {
+        [Required]
         [Attr("name")]
         public string Name { get; set; }
 
         [Attr("description")]
         public string Description { get; set; } = "";
 
+        [Required]
         [Attr("priority")]
         public int Priority { get; set; }
 
         public int DistributedTaskDefinitionId { get; set; }
 
+        [Required]
         [HasOne("distributed-task-definition")]
-        public DistributedTaskDefinition DistributedTaskDefinition { get; set; }
+        public virtual DistributedTaskDefinition DistributedTaskDefinition { get; set; }
 
+        [Required]
         [Attr("input-data", isImmutable: true, isFilterable: false, isSortable: false)]
         public string InputData { get; set; }
 
         [Attr("status", isImmutable: true)]
-        public DistributedTaskStatus Status { get; set; }
+        public DistributedTaskStatus Status { get; set; } = DistributedTaskStatus.InProgress;
 
         [Attr("result", isImmutable: true)]
         public string Result { get; set; }
 
         [HasMany("subtasks")]
-        public List<Subtask> Subtasks { get; set; }
+        public virtual List<Subtask> Subtasks { get; set; }
     }
 }
--- Server/Models/Subtask.cs
+++ Server/Models/Subtask.cs
@@ -1,26 +1,31 @@
+using System.ComponentModel.DataAnnotations;
 using JsonApiDotNetCore.Models;
 
 namespace Server.Models
 {
     public class Subtask : Identifiable
     {
+        [Required]
         [Attr("sequence-number", isImmutable: true)]
         public int SequenceNumber { get; set; }
 
+        [Required]
         public string Token { get; set; }
 
-        [Attr("distributed-task-id", isImmutable: true)]
         public int DistributedTaskId { get; set; }
 
+        [Required]
         [HasOne("distributed-task")]
         public DistributedTask DistributedTask { get; set; }
 
+        [Required]
         [Attr("input-data", isImmutable: true)]
         public string InputData { get; set; }
 
         [Attr("result", isImmutable: true)]
         public string Result { get; set; }
 
+        [Required]
         [Attr("subtask-status", isImmutable: true)]
         public SubtaskStatus Status { get; set; }
     }
