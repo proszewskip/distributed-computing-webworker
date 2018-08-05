99c487de82c50d05a7f8b81ad239a634f225b170
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-08-05 13:17:10 +0200

    [server] add Required annotations in models

--- Server/Models/DistributedTaskDefinition.cs
+++ Server/Models/DistributedTaskDefinition.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using JsonApiDotNetCore.Models;
 
 namespace Server.Models
 {
     public class DistributedTaskDefinition : Identifiable
     {
+        [Required]
         [Attr("name")]
         public string Name { get; set; }
 
@@ -16,14 +18,17 @@ namespace Server.Models
         public List<DistributedTask> DistributedTasks { get; set; }
 
         // TODO: find out if DefintionGuid needs to be public
+        [Required]
         [Attr("definition-guid", isImmutable: true)]
         public Guid DefinitionGuid { get; set; }
 
         // TODO: find out if MainDllName needs to be public
+        [Required]
         [Attr("main-dll-name", isImmutable: true)]
         public string MainDllName { get; set; }
 
         [Attr("subtask-info", isImmutable: true)]
+        [Required]
         public SubtaskInfo SubtaskInfo { get; set; }
     }
 }
