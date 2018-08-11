b289872e372e50713fb547dd1b88b733e7f803b5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-08-11 19:07:29 +0200

    [server] move variables from .env to appsettings.json

---
+++ Server/Models/ServerConfig.cs
@@ -0,0 +1,9 @@
+namespace Server.Models
+{
+    public class ServerConfig
+    {
+        public string MonoPackagerPath { get; set; }
+        public string TaskDefinitionsDirectoryPath { get; set; }
+        public string CompiledTaskDefinitionsDirectoryPath { get; set; }
+    }
+}
