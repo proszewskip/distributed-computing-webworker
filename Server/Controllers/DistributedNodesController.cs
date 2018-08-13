db7f0e45b40edcb5d15df1dac8d05e7f96798aea
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-08-13 11:07:38 +0200

    [server] add a controller for distributed nodes

---
+++ Server/Controllers/DistributedNodesController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading.Tasks;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    public class DistributedNodesController : BaseJsonApiController<DistributedNode, Guid>
+    {
+        private readonly IResourceService<DistributedNode, Guid> _resourceService;
+
+        public DistributedNodesController(
+            IJsonApiContext jsonApiContext,
+            IResourceService<DistributedNode, Guid> resourceService
+        ) : base(jsonApiContext, resourceService)
+        {
+            _resourceService = resourceService;
+        }
+
+        [HttpPost("register")]
+        public Task<DistributedNode> Register()
+        {
+            var distributedNode = new DistributedNode
+            {
+                Id = Guid.NewGuid(),
+                LastKeepAliveTime = DateTime.Now
+            };
+
+            return _resourceService.CreateAsync(distributedNode);
+        }
+
+        [HttpPost("{id}/keep-alive")]
+        public async Task<ActionResult> KeepAlive(Guid id)
+        {
+            var distributedNode = await _resourceService.GetAsync(id);
+
+            if (distributedNode == null)
+                return NotFound();
+
+            distributedNode.LastKeepAliveTime = DateTime.Now;
+            await _resourceService.UpdateAsync(id, distributedNode);
+
+            return Ok(distributedNode);
+        }
+    }
+}
