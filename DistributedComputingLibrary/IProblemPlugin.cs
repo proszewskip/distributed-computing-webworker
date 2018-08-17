87d7ff3c0616bc42c4e4b221a654cdf3cc11e399
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-08-17 20:22:18 +0200

    [server] add IProblemPlugin type constraints

--- DistributedComputingLibrary/IProblemPlugin.cs
+++ DistributedComputingLibrary/IProblemPlugin.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 namespace DistributedComputing
 {
     public interface IProblemPlugin<TTask, TTaskResult, TSubtask, out TSubtaskResult>
+        where TTask : class
+        where TTaskResult : class
+        where TSubtask : class
+        where TSubtaskResult : class
     {
         TTask ParseTask(byte[] data);
 
