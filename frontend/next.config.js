46f16a6cc41ac36f2b67fcc05f9531a34a675a28
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-10-20 18:18:54 +0200

    [frontend] add react-table, allow including css

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,13 +2,16 @@
 
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const withCSS = require('@zeit/next-css');
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    if (options.isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin());
-    }
+module.exports = withCSS(
+  withTypescript({
+    webpack(config, options) {
+      if (options.isServer) {
+        config.plugins.push(new ForkTsCheckerWebpackPlugin());
+      }
 
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
