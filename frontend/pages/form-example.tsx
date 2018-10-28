e12c649e31b92e1baef83aefe42c0fdc7b0e4bfd
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-10-28 22:56:32 +0100

    [frontend] add errors handling to form prototype

--- frontend/pages/form-example.tsx
+++ frontend/pages/form-example.tsx
@@ -1,16 +1,26 @@
 import FormData from 'form-data';
-import { Field, Form, Formik, FormikActions } from 'formik';
+import { ErrorMessage, Field, Form, Formik, FormikActions } from 'formik';
 import fetch from 'isomorphic-unfetch';
 import React, { Component } from 'react';
+import * as Yup from 'yup';
+
 const serverIp = 'http://localhost:5000';
 const entityPath = '/distributed-task-definitions/add';
 const urlToFetch = `${serverIp}${entityPath}`;
 
+const SignUpSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Must be longer than 3 characters')
+    .required('Required'),
+  // 'main-dll': Yup.object().required('Required'),
+  // 'additional-dlls': Yup.object().required('Required'),
+});
+
 interface DistributedTaskDefinition {
   name: string;
   description: string;
-  'main-dll': File | null;
-  'additional-dlls': FileList | null;
+  MainDll: File | undefined;
+  'additional-dlls': FileList | undefined;
 }
 
 interface FormExampleProps {
@@ -22,32 +32,58 @@ class FormExample extends Component<FormExampleProps> {
     const initialValues: DistributedTaskDefinition = {
       name: '',
       description: '',
-      'main-dll': null,
-      'additional-dlls': null,
+      MainDll: undefined,
+      AdditionalDlls: undefined,
     };
 
     return (
       <Formik
         initialValues={initialValues}
+        validationSchema={SignUpSchema}
         /* tslint:disable:jsx-no-multiline-js jsx-no-lambda*/
 
         onSubmit={(
           values: DistributedTaskDefinition,
-          { setSubmitting }: FormikActions<DistributedTaskDefinition>,
+          {
+            setSubmitting,
+            setErrors,
+            setTouched,
+          }: FormikActions<DistributedTaskDefinition>,
         ) => {
           setTimeout(async () => {
+            setTouched({
+              MainDll: true,
+              AdditionalDlls: true,
+            });
+
             setSubmitting(false);
             const data = new FormData();
-            data.append('MainDll', values['main-dll']);
+            data.append('MainDll', values.MainDll);
 
-            for (const file of values['additional-dlls']) {
-              data.append('AdditionalDlls', file);
+            if (values['AdditionalDlls'] !== undefined) {
+              for (const file of values['AdditionalDlls']) {
+                data.append('AdditionalDlls', file);
+              }
             }
             data.append('name', values.name);
 
             await fetch(urlToFetch, {
               method: 'post',
               body: data,
+            }).then(async (response: any) => {
+              if (!response.ok) {
+                const result = await response.json();
+
+                const errorObject = {};
+
+                for (const error of result.Errors) {
+                  errorObject[error.title] = error.detail;
+                }
+
+                setErrors(errorObject);
+
+                alert(JSON.stringify(errorObject));
+              }
             });
           }, 500);
         }}
@@ -60,6 +96,9 @@ class FormExample extends Component<FormExampleProps> {
               placeholder="Distributed Task Definition Name"
               type="text"
             />
+            <ErrorMessage name="name">
+              {(errorMessage) => <div className="error">{errorMessage}</div>}
+            </ErrorMessage>
 
             <label htmlFor="description">Description</label>
             <Field
@@ -69,30 +108,35 @@ class FormExample extends Component<FormExampleProps> {
               type="text"
             />
 
-            <label htmlFor="distributedTaskDefinitionMainDll">Main Dll</label>
+            <label htmlFor="main-dll">Main Dll</label>
             <Field
-              id="distributedTaskDefinitionMainDll"
-              name="distributedTaskDefinitionMainDll"
+              id="main-dll"
+              name="main-dll"
               type="file"
               accept=".dll"
               onChange={(event: any) => {
-                setFieldValue('main-dll', event.currentTarget.files[0]);
+                setFieldValue('MainDll', event.currentTarget.files[0]);
               }}
             />
+            <ErrorMessage name="MainDll">
+              {(errorMessage) => <div className="error">{errorMessage}</div>}
+            </ErrorMessage>
 
-            <label htmlFor="distributedTaskDefinitionAdditionalDlls">
-              Additional Dlls
-            </label>
+            <label htmlFor="additional-dlls">Additional Dlls</label>
             <Field
-              id="distributedTaskDefinitionMainDll"
-              name="distributedTaskDefinitionMainDll"
+              id="additional-dlls"
+              name="additional-dlls"
               type="file"
               multiple={true}
               accept=".dll"
               onChange={(event: any) => {
-                setFieldValue('additional-dlls', event.currentTarget.files);
+                setFieldValue('AdditionalDlls', event.currentTarget.files);
               }}
             />
+
+            <ErrorMessage name="AdditionalDlls">
+              {(errorMessage) => <div className="error">{errorMessage}</div>}
+            </ErrorMessage>
 
             <button type="submit" style={{ display: 'block' }}>
               Submit
