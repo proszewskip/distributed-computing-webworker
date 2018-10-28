f32b19fb23daeb95b7102c142d4b53fdba27e154
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-10-28 15:53:34 +0100

    [frontend] fix module resolution

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,7 @@
 // NOTE: https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
 
+const path = require('path');
+
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const withCSS = require('@zeit/next-css');
@@ -10,6 +12,11 @@ module.exports = withCSS(
       if (options.isServer) {
         config.plugins.push(new ForkTsCheckerWebpackPlugin());
       }
+
+      config.resolve.modules = [
+        path.resolve(__dirname, 'src'),
+        ...config.resolve.modules,
+      ];
 
       return config;
     },
