4ab8dcaac35b3c2ef834c6f6a3bc31a7ab2e6bd6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-10-28 19:23:40 +0100

    [frontend] add Entity model

--- frontend/src/models/distributed-task-definition.ts
+++ frontend/src/models/distributed-task-definition.ts
@@ -1,7 +1,7 @@
+import { Entity } from './entity';
 import { ProblemPluginInfo } from './problem-plugin-info';
 
-export interface DistributedTaskDefinition {
-  id: string;
+export interface DistributedTaskDefinition extends Entity {
   name: string;
   description: string;
   'definition-guid': string;
