dd79f5b4b0a3d3054998a745432af746fdf3a874
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-11-02 22:20:45 +0100

    [frontend] refactor form example

--- frontend/pages/form-example.tsx
+++ frontend/pages/form-example.tsx
@@ -1,151 +1,176 @@
-import FormData from 'form-data';
-import { ErrorMessage, Field, Form, Formik, FormikActions } from 'formik';
+import { Button, TextInput } from 'evergreen-ui';
+import { FormikActions, FormikProps, withFormik } from 'formik';
 import fetch from 'isomorphic-unfetch';
-import React, { Component } from 'react';
+import React from 'react';
 import * as Yup from 'yup';
+
+import { CreateDistributedTaskDefinition } from '../src/models/index';
+
+import FilePicker from '../src/components/form/file-picker';
+import ValidatedFormField from '../src/components/form/validated-form-field';
 
 const serverIp = 'http://localhost:5000';
 const entityPath = '/distributed-task-definitions/add';
 const urlToFetch = `${serverIp}${entityPath}`;
 
-const SignUpSchema = Yup.object().shape({
+const ValidationSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, 'Must be longer than 3 characters')
     .required('Required'),
-  // 'main-dll': Yup.object().required('Required'),
-  // 'additional-dlls': Yup.object().required('Required'),
+  MainDll: Yup.object()
+    .nullable(true)
+    .test('Defined', 'MainDll is required', (file: any) => file !== undefined),
+  AdditionalDlls: Yup.array().required(),
 });
 
-interface DistributedTaskDefinition {
-  name: string;
-  description: string;
-  MainDll: File | undefined;
-  'additional-dlls': FileList | undefined;
-}
+const ExampleForm = ({
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+  setFieldValue,
+  setTouched,
+}: FormikProps<CreateDistributedTaskDefinition>) => (
+  <form onSubmit={handleSubmit}>
+    <ValidatedFormField
+      label="Name"
+      labelFor="name"
+      isRequired={true}
+      isErrorVisible={touched.name === true}
+      validationResult={errors.name}
+    />
+    <TextInput
+      id="name"
+      name="name"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.name}
+      width="30rem"
+    />
 
-interface FormExampleProps {
-  data: DistributedTaskDefinition[];
-}
+    <ValidatedFormField
+      label="Description"
+      labelFor="description"
+      isRequired={true}
+    />
+    <TextInput
+      id="description"
+      name="description"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.description}
+      height="6rem"
+      width="30rem"
+    />
 
-class FormExample extends Component<FormExampleProps> {
-  public render() {
-    const initialValues: DistributedTaskDefinition = {
-      name: '',
-      description: '',
-      MainDll: undefined,
-      AdditionalDlls: undefined,
-    };
+    <ValidatedFormField
+      label="MainDll"
+      labelFor="MainDll"
+      isRequired={true}
+      isErrorVisible={touched.MainDll === true}
+      validationResult={errors.MainDll}
+    />
+    <FilePicker
+      name="MainDll"
+      // tslint:disable-next-line:jsx-no-multiline-js jsx-no-lambda
+      onChange={(fileList: FileList) => {
+        setFieldValue('MainDll', fileList[0], true);
+        setTouched({
+          MainDll: true,
+          ...touched,
+        });
+      }}
+      accept=".dll"
+      required={true}
+    />
 
-    return (
-      <Formik
-        initialValues={initialValues}
-        validationSchema={SignUpSchema}
-        /* tslint:disable:jsx-no-multiline-js jsx-no-lambda*/
+    <ValidatedFormField
+      label="AdditionalDlls"
+      labelFor="AdditionalDlls"
+      isRequired={true}
+      isErrorVisible={touched.AdditionalDlls === true}
+      validationResult={errors.AdditionalDlls}
+    />
+    <FilePicker
+      name="AdditionalDlls"
+      // tslint:disable-next-line:jsx-no-multiline-js jsx-no-lambda
+      onChange={(fileList: FileList) => {
+        setFieldValue('AdditionalDlls', fileList, true);
+        setTouched({
+          AdditionalDlls: true,
+          ...touched,
+        });
+      }}
+      multiple={true}
+      accept=".dll"
+    />
 
-        onSubmit={(
-          values: DistributedTaskDefinition,
-          {
-            setSubmitting,
-            setErrors,
-            setTouched,
-          }: FormikActions<DistributedTaskDefinition>,
-        ) => {
-          setTimeout(async () => {
-            setTouched({
-              MainDll: true,
-              AdditionalDlls: true,
-            });
+    <Button type="submit" disabled={isSubmitting}>
+      Submit
+    </Button>
+  </form>
+);
 
-            setSubmitting(false);
-            const data = new FormData();
-            data.append('MainDll', values.MainDll);
+const ExampleFormWithFormik = withFormik({
+  mapPropsToValues: () => ({
+    name: '',
+    description: '',
+    MainDll: undefined,
+    AdditionalDlls: undefined,
+  }),
+  validationSchema: ValidationSchema,
+  handleSubmit: (
+    values: CreateDistributedTaskDefinition,
+    {
+      setSubmitting,
+      setErrors,
+    }: FormikActions<CreateDistributedTaskDefinition>,
+  ) => {
+    setTimeout(async () => {
+      setSubmitting(true);
+      const formData = new FormData();
+      if (values.MainDll !== undefined) {
+        formData.append('MainDll', values.MainDll);
+      }
 
-            if (values['AdditionalDlls'] !== undefined) {
-              for (const file of values['AdditionalDlls']) {
-                data.append('AdditionalDlls', file);
-              }
-            }
-            data.append('name', values.name);
+      if (values.AdditionalDlls !== undefined) {
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < values.AdditionalDlls.length; ++i) {
+          formData.append('AdditionalDlls', values.AdditionalDlls[i]);
+        }
+      }
 
-            await fetch(urlToFetch, {
-              method: 'post',
-              body: data,
-            }).then(async (response: any) => {
-              if (!response.ok) {
-                const result = await response.json();
+      formData.append('name', values.name);
 
-                const errorObject = {};
+      await fetch(urlToFetch, {
+        method: 'post',
+        body: formData,
+      }).then(async (response: Response) => {
+        if (!response.ok) {
+          const result = await response.json();
 
-                for (const error of result.Errors) {
-                  errorObject[error.title] = error.detail;
-                }
+          const errorObject: any = {};
 
-                setErrors(errorObject);
+          for (const error of result.Errors) {
+            errorObject[error.Title] = error.detail;
+          }
 
-                alert(JSON.stringify(errorObject));
-              }
-            });
-          }, 500);
-        }}
-        render={({ setFieldValue }) => (
-          <Form>
-            <label htmlFor="name">Name</label>
-            <Field
-              id="name"
-              name="name"
-              placeholder="Distributed Task Definition Name"
-              type="text"
-            />
-            <ErrorMessage name="name">
-              {(errorMessage) => <div className="error">{errorMessage}</div>}
-            </ErrorMessage>
+          setErrors(errorObject);
+        }
+        setSubmitting(false);
+      });
+    }, 500);
+  },
+})(ExampleForm);
 
-            <label htmlFor="description">Description</label>
-            <Field
-              id="description"
-              name="description"
-              placeholder="Distributed Task Definition Description"
-              type="text"
-            />
+const Basic = () => (
+  <div>
+    <h1>Example form</h1>
+    <ExampleFormWithFormik />
+  </div>
+);
 
-            <label htmlFor="main-dll">Main Dll</label>
-            <Field
-              id="main-dll"
-              name="main-dll"
-              type="file"
-              accept=".dll"
-              onChange={(event: any) => {
-                setFieldValue('MainDll', event.currentTarget.files[0]);
-              }}
-            />
-            <ErrorMessage name="MainDll">
-              {(errorMessage) => <div className="error">{errorMessage}</div>}
-            </ErrorMessage>
-
-            <label htmlFor="additional-dlls">Additional Dlls</label>
-            <Field
-              id="additional-dlls"
-              name="additional-dlls"
-              type="file"
-              multiple={true}
-              accept=".dll"
-              onChange={(event: any) => {
-                setFieldValue('AdditionalDlls', event.currentTarget.files);
-              }}
-            />
-
-            <ErrorMessage name="AdditionalDlls">
-              {(errorMessage) => <div className="error">{errorMessage}</div>}
-            </ErrorMessage>
-
-            <button type="submit" style={{ display: 'block' }}>
-              Submit
-            </button>
-          </Form>
-        )}
-      />
-    );
-  }
-}
-
-export default FormExample;
+export default Basic;
