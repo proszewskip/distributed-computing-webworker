e02ca47baf6b760425f1a1388fd81531bf8d8152
budget <budget@localhost.localdomain>
2018-11-02 21:45:13 +0100

    [frontend] add custom file picker

---
+++ frontend/src/components/form/file-picker.tsx
@@ -0,0 +1,131 @@
+import { Button, TextInput } from 'evergreen-ui';
+import React, { PureComponent } from 'react';
+import Box from 'ui-box';
+
+export const CLASS_PREFIX = 'evergreen-file-picker';
+
+interface FilePickerProps {
+  name?: string;
+  accept?: string | [string];
+  required?: boolean;
+  multiple?: boolean;
+  disabled?: boolean;
+  capture?: boolean;
+  height?: number;
+  onChange?: (arg: any) => any;
+}
+
+interface FilePickerState {
+  files: any[];
+}
+
+export default class FilePicker extends PureComponent<
+  FilePickerProps,
+  FilePickerState
+> {
+  public fileInput: any;
+  constructor(props: FilePickerProps) {
+    super(props);
+    this.state = {
+      files: [],
+    };
+  }
+
+  public render() {
+    const {
+      name,
+      accept,
+      required,
+      multiple,
+      disabled,
+      capture,
+      height,
+      onChange,
+      // tslint:disable-next-line:trailing-comma
+      ...props
+    } = this.props;
+    const { files } = this.state;
+
+    let inputValue;
+    if (files.length === 0) {
+      inputValue = '';
+    } else if (files.length === 1) {
+      inputValue = files[0].name;
+    } else {
+      inputValue = `${files.length} files`;
+    }
+
+    let buttonText;
+    if (files.length === 0) {
+      buttonText = 'Select file';
+    } else if (files.length === 1) {
+      buttonText = 'Replace file';
+    } else {
+      buttonText = 'Replace files';
+    }
+
+    return (
+      <Box display="flex" className={`${CLASS_PREFIX}-root`} {...props}>
+        <Box
+          innerRef={this.fileInputRef}
+          className={`${CLASS_PREFIX}-file-input`}
+          is="input"
+          type="file"
+          name={name}
+          accept={accept}
+          required={required}
+          multiple={multiple}
+          disabled={disabled}
+          capture={capture}
+          onChange={this.handleFileChange}
+          display="none"
+        />
+
+        <TextInput
+          className={`${CLASS_PREFIX}-text-input`}
+          readOnly={true}
+          value={inputValue}
+          placeholder="Select a file to upload…"
+          borderTopRightRadius="0 !important"
+          borderBottomRightRadius="0 !important"
+          height={height}
+          flex={1}
+          textOverflow="ellipsis"
+        />
+
+        <Button
+          className={`${CLASS_PREFIX}-button`}
+          onClick={this.handleButtonClick}
+          disabled={disabled}
+          borderTopLeftRadius={0}
+          borderBottomLeftRadius={0}
+          height={height}
+          flexShrink={0}
+          type="button"
+        >
+          {buttonText}
+        </Button>
+      </Box>
+    );
+  }
+
+  public fileInputRef = (node: any) => {
+    this.fileInput = node;
+  };
+
+  public handleFileChange = (e: any) => {
+    const { onChange } = this.props;
+
+    const files = [...e.target.files];
+
+    this.setState({ files });
+
+    if (onChange) {
+      onChange(files);
+    }
+  };
+
+  public handleButtonClick = () => {
+    this.fileInput.click();
+  };
+}
