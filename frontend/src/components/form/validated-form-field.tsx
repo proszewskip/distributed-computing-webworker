deb755bfa93f7fe475a8e9014c3fef7846e66335
proszewskip <przemek.proszewski@gmail.com>
2018-11-02 22:20:45 +0100

    [frontend] refactor form example

--- frontend/src/components/form/validated-form-field.tsx
+++ frontend/src/components/form/validated-form-field.tsx
@@ -2,7 +2,7 @@ import { FormField, InlineAlert } from 'evergreen-ui';
 import React from 'react';
 
 interface ValidatedFormFieldProps {
-  visible: boolean;
+  isErrorVisible?: boolean;
   validationResult?: string;
   label: string;
   labelFor: string;
@@ -13,13 +13,13 @@ export default class ValidatedFormField extends React.Component<
   ValidatedFormFieldProps
 > {
   public render() {
-    const { visible, validationResult, ...props } = this.props;
+    const { isErrorVisible, validationResult, ...props } = this.props;
 
     return (
       /* tslint:disable:jsx-no-multiline-js */
       <div>
         <FormField {...props} />
-        {visible &&
+        {isErrorVisible &&
           validationResult && (
             <InlineAlert intent="danger" marginBottom={32}>
               {validationResult}
--- frontend/src/types/evergreen-ui.d.ts
+++ frontend/src/types/evergreen-ui.d.ts
@@ -1,2 +1,1 @@
 declare module 'evergreen-ui';
-declare module 'ui-box';
