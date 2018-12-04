491db05c5165106c88e1f35745aa5b31994df851
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-04 23:16:59 +0100

    [frontend] add mobile sidebar in Layout

--- frontend/src/components/layout/layout.tsx
+++ frontend/src/components/layout/layout.tsx
@@ -1,25 +1,91 @@
-import { Pane } from 'evergreen-ui';
-import React, { ReactNode, StatelessComponent } from 'react';
+import { IconButton, majorScale, Pane } from 'evergreen-ui';
+import React, { PureComponent, ReactNode, MouseEventHandler } from 'react';
 
-interface LayoutProps {
+import { Desktop, Mobile } from 'components/responsive-helpers';
+
+export interface LayoutProps {
   renderSidebar: () => ReactNode;
 }
 
-export const Layout: StatelessComponent<LayoutProps> = ({
-  children,
-  renderSidebar,
-}) => {
-  return (
-    <Pane width="100%" height="100%" display="flex">
-      <Pane width={200} height="100%">
-        {renderSidebar()}
-      </Pane>
+interface LayoutState {
+  isSidebarShown: boolean;
+}
 
-      <Pane flex={1} overflow="auto">
-        {children}
+export class Layout extends PureComponent<LayoutProps, LayoutState> {
+  public state: LayoutState = {
+    isSidebarShown: false,
+  };
+
+  public render() {
+    return (
+      <>
+        <Mobile>
+          {this.state.isSidebarShown && this.renderMobileSidebar()}
+
+          <Pane
+            display="flex"
+            flexDirection="column"
+            width="100%"
+            height="100%"
+          >
+            <IconButton
+              icon="menu"
+              onClick={this.openSidebar}
+              margin={majorScale(2)}
+            />
+
+            {this.renderMain()}
+          </Pane>
+        </Mobile>
+
+        <Desktop>
+          <Pane display="flex" height="100%">
+            <Pane width={200} height="100%">
+              {this.props.renderSidebar()}
+            </Pane>
+
+            {this.renderMain()}
+          </Pane>
+        </Desktop>
+      </>
+    );
+  }
+
+  private renderMobileSidebar = (): ReactNode => (
+    <Pane
+      position="absolute"
+      top={0}
+      left={0}
+      width="100%"
+      height="100%"
+      zIndex={100}
+      onClick={this.closeSidebar}
+      background="overlay"
+    >
+      <Pane onClick={this.stopPropagation} height="100%" width={200}>
+        {this.props.renderSidebar()}
       </Pane>
     </Pane>
   );
-};
 
-export default Layout;
+  private renderMain = (): ReactNode => (
+    <Pane flex={1} overflow="auto">
+      {this.props.children}
+    </Pane>
+  );
+
+  private openSidebar = () => {
+    this.setState({
+      isSidebarShown: true,
+    });
+  };
+
+  private closeSidebar = () => {
+    this.setState({
+      isSidebarShown: false,
+    });
+  };
+
+  private stopPropagation: MouseEventHandler = (event) =>
+    event.stopPropagation();
+}
