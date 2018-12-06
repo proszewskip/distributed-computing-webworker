272705048d7dbb9e71a757dd3404699583835819
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-06 09:57:30 +0100

    [frontend] apply layout to the index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,20 +1,46 @@
+import { Card, Pane } from 'evergreen-ui';
 import React from 'react';
 
-import Link from 'next/link';
+import 'styles.css';
+
+import { Layout, LayoutProps } from 'components/layout';
+import { Sidebar, SidebarMenu } from 'components/sidebar';
+import { SidebarMenuProps } from 'components/sidebar';
+
+const always = <T extends {}>(v: T) => () => v;
+
+export const sidebarMenuItems: SidebarMenuProps['items'] = [
+  {
+    href: '/home',
+    title: 'Home',
+    isActive: always(false),
+  },
+  {
+    href: '/table-example',
+    title: 'Table example',
+    isActive: always(false),
+  },
+  {
+    href: '/form-example',
+    title: 'Form example',
+    isActive: always(false),
+  },
+];
+
+const renderSidebar: LayoutProps['renderSidebar'] = () => (
+  <Sidebar title="Distributed Computing">
+    <SidebarMenu items={sidebarMenuItems} />
+  </Sidebar>
+);
 
 const Index = () => (
-  <div>
-    <Link href="/home">
-      <a>Home</a>
-    </Link>
-    <Link href="/table-example">
-      <a>Table example</a>
-    </Link>
-    <Link href="/form-example">
-      <a>Form example</a>
-    </Link>
-    Hello Next.js
-  </div>
+  <Layout renderSidebar={renderSidebar}>
+    <Pane display="flex" justifyContent="center" marginTop="2em">
+      <Card padding={80} border="default" background="tint2">
+        Info here
+      </Card>
+    </Pane>
+  </Layout>
 );
 
 export default Index;
