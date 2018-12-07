a2ec5b5c59f00903a29e91edb6c0935434a51c8f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-07 08:52:47 +0100

    [frontend] reduce max mobile width

--- frontend/src/components/responsive-helpers/responsive-helpers.tsx
+++ frontend/src/components/responsive-helpers/responsive-helpers.tsx
@@ -1,7 +1,7 @@
 import React, { StatelessComponent } from 'react';
 import MediaQuery, { MediaQueryProps } from 'react-responsive';
 
-const maxMobileWidth = 767;
+const maxMobileWidth = 599;
 const minDesktopWidth = maxMobileWidth + 1;
 
 export const Mobile: StatelessComponent<MediaQueryProps> = (props) => (
