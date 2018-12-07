8d0e900478ec740ec50d023d0e14633d69efc4e3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-07 08:49:36 +0100

    [frontend] add viewport and title to the index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Card, Pane } from 'evergreen-ui';
+import Head from 'next/head';
 import { always } from 'ramda';
 import React from 'react';
 
@@ -33,13 +34,20 @@ const renderSidebar: LayoutProps['renderSidebar'] = () => (
 );
 
 const Index = () => (
-  <Layout renderSidebar={renderSidebar}>
-    <Pane display="flex" justifyContent="center" marginTop="2em">
-      <Card padding={80} border="default" background="tint2">
-        Info here
-      </Card>
-    </Pane>
-  </Layout>
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Distributed Computing</title>
+    </Head>
+
+    <Layout renderSidebar={renderSidebar}>
+      <Pane display="flex" justifyContent="center" marginTop="2em">
+        <Card padding={80} border="default" background="tint2">
+          Info here
+        </Card>
+      </Pane>
+    </Layout>
+  </>
 );
 
 export default Index;
