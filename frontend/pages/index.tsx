6cf57d957af4b810bf2c407422258cfe93e94cff
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-08 11:51:10 +0100

    [frontend] use AuthenticatedSidebar on the index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,36 +1,14 @@
 import { Card, Pane } from 'evergreen-ui';
-import { always } from 'ramda';
 import React from 'react';
 
 import 'styles.css';
 
 import { Head } from 'components/head';
 import { Layout, LayoutProps } from 'components/layout';
-import { Sidebar, SidebarMenu } from 'components/sidebar';
-import { SidebarMenuProps } from 'components/sidebar';
-
-export const sidebarMenuItems: SidebarMenuProps['items'] = [
-  {
-    href: '/home',
-    title: 'Home',
-    isActive: always(false),
-  },
-  {
-    href: '/table-example',
-    title: 'Table example',
-    isActive: always(false),
-  },
-  {
-    href: '/form-example',
-    title: 'Form example',
-    isActive: always(false),
-  },
-];
+import { AuthenticatedSidebar } from 'product-specific';
 
 const renderSidebar: LayoutProps['renderSidebar'] = () => (
-  <Sidebar title="Distributed Computing">
-    <SidebarMenu items={sidebarMenuItems} />
-  </Sidebar>
+  <AuthenticatedSidebar />
 );
 
 const Index = () => (
