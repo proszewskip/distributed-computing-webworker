8d42531e49cbf37f1c0e7609d9779b603a7d2f31
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-08 20:56:27 +0100

    [frontend] add jest mocks for files and styles

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -11,4 +11,9 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.ts',
+    '\\.(css|less)$': '<rootDir>/__mocks__/style-mock.ts',
+  },
 };
