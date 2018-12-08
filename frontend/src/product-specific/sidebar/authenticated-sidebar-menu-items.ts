7214d1ed1e57596d8b14659aa0e66d6f56dc8618
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-08 12:10:25 +0100

    [frontend] adjust authenticated sidebar menu items
    Remove the home page
    Move index page to the top

--- frontend/src/product-specific/sidebar/authenticated-sidebar-menu-items.ts
+++ frontend/src/product-specific/sidebar/authenticated-sidebar-menu-items.ts
@@ -2,19 +2,15 @@ import { ConnectedMenuItem } from 'components/sidebar';
 
 export const authenticatedSidebarMenuItems: ConnectedMenuItem[] = [
   {
+    href: '/',
+    title: 'Home',
+  },
+  {
     href: '/form-example',
     title: 'Form example',
   },
   {
     href: '/table-example',
     title: 'Table example',
-  },
-  {
-    href: '/home',
-    title: 'Home',
-  },
-  {
-    href: '/',
-    title: 'Index',
   },
 ];
