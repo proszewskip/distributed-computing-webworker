c49eed836d01dc647ea3e80d2ff74e7377747834
proszewskip <przemek.proszewski@gmail.com>
2018-12-09 23:24:06 +0100

    [frontend] rename files, move files to correct locations

--- frontend/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const config = {
-  serverIp: 'http://localhost:5000',
-};
--- frontend/src/product-specific/index.ts
+++ frontend/src/product-specific/index.ts
@@ -3,4 +3,3 @@ export * from './head';
 export * from './kitsu';
 export * from './config';
 export * from './dependency-injection';
-export * from './next-router';
