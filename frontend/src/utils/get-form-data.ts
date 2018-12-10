1574674b6a78752f5994c23b48cb6aa5bb462474
proszewskip <przemek.proszewski@gmail.com>
2018-12-10 01:37:30 +0100

    [frontend] handle arrays in get form data

--- frontend/src/utils/get-form-data.ts
+++ frontend/src/utils/get-form-data.ts
@@ -1,8 +1,15 @@
 export function getFormData(model: object) {
   const formData = new FormData();
 
-  for (const entry of Object.entries(model)) {
-    formData.append(entry[0], entry[1]);
+  for (const [property, value] of Object.entries(model)) {
+    if (Array.isArray(value)) {
+      for (const item of value) {
+        formData.append(property, item);
+      }
+      continue;
+    }
+
+    formData.append(property, value);
   }
 
   return formData;
