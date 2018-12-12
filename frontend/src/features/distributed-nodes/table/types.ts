d98d5bf2a944d1a8ad4c0de63f6fccb10fa46a09
proszewskip <przemek.proszewski@gmail.com>
2018-12-12 19:12:00 +0100

    [frontend] extract nodes table types, remove unused elements

--- frontend/src/features/distributed-nodes/table/types.ts
+++ frontend/src/features/distributed-nodes/table/types.ts
@@ -1,6 +1,20 @@
+import { List } from 'immutable';
 import { Entity } from 'models';
+import { Omit } from 'ramda';
 
 export interface DistributedNodeModel extends Entity {
   'trust-level': number;
   'last-keep-alive-time': number;
+}
+
+export interface DistributedNodesTableProps {
+  data: DistributedNodeModel[];
+  totalRecordsCount: number;
+}
+
+export interface DistributedNodesTableState
+  extends Omit<DistributedNodesTableProps, 'data'> {
+  data: List<DistributedNodeModel>;
+  loading: boolean;
+  filteringEnabled: boolean;
 }
