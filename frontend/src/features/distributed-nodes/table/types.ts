67127b179b777ee7705090756c34cd13d68868c2
proszewskip <przemek.proszewski@gmail.com>
2018-12-12 20:32:15 +0100

    [frontend] apply fix for the refresh button

--- frontend/src/features/distributed-nodes/table/types.ts
+++ frontend/src/features/distributed-nodes/table/types.ts
@@ -2,6 +2,8 @@ import { List } from 'immutable';
 import { Entity } from 'models';
 import { Omit } from 'ramda';
 
+import { ForceFetchData } from 'components/data-table/data-table';
+
 export interface DistributedNodeModel extends Entity {
   'trust-level': number;
   'last-keep-alive-time': number;
@@ -17,4 +19,5 @@ export interface DistributedNodesTableState
   data: List<DistributedNodeModel>;
   loading: boolean;
   filteringEnabled: boolean;
+  forceFetchDataCallback: ForceFetchData;
 }
