2bcd9905248cc9bc53153933df7d913041ddb459
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-15 13:50:07 +0100

    [frontend] add distributed task definitions table, remove example table

---
+++ frontend/src/features/distributed-task-definitions/table/types.ts
@@ -0,0 +1,29 @@
+import { List } from 'immutable';
+import Kitsu from 'kitsu';
+import { Omit } from 'lodash';
+
+import { DistributedTaskDefinition } from 'models';
+
+import { ForceFetchData } from 'components/data-table/data-table';
+import { WithSelectableRowsAdditionalProps } from 'components/data-table/with-selectable-rows';
+
+export interface DistributedTaskDefinitionsTableOwnProps {
+  data: DistributedTaskDefinition[];
+  totalRecordsCount: number;
+}
+
+export interface DistributedTaskDefinitionsTableDependencies {
+  kitsu: Kitsu;
+}
+
+export type DistributedTaskDefinitionsTableProps = DistributedTaskDefinitionsTableOwnProps &
+  DistributedTaskDefinitionsTableDependencies;
+
+export interface DistributedTaskDefinitionsTableState
+  extends Omit<DistributedTaskDefinitionsTableOwnProps, 'data'> {
+  data: List<DistributedTaskDefinition>;
+  loading: boolean;
+  selectedRowIds: WithSelectableRowsAdditionalProps['selectedRowIds'];
+  filteringEnabled: boolean;
+  forceFetchDataCallback: ForceFetchData;
+}
