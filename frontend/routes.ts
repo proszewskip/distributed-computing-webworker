a10eb442a871bffe06c2ad9a782582d46b879028
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-15 13:50:46 +0100

    [frontend] add missing distributed task definitions routes

--- frontend/routes.ts
+++ frontend/routes.ts
@@ -3,6 +3,18 @@ import Routes from 'next-routes';
 export const routes = new Routes();
 
 routes.add(
+  'create-distributed-task-definition',
+  '/distributed-task-definitions/create',
+  'distributed-task-definitions/create',
+);
+
+routes.add(
+  'update-distributed-task-definition',
+  '/distributed-task-definitions/:id/update',
+  'distributed-task-definitions/update',
+);
+
+routes.add(
   'distributed-task-definition-details',
   '/distributed-task-definitions/:id',
   'distributed-task-definitions/details',
