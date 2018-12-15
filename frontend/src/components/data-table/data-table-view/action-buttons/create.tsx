879836eb9d7a4ef3aefdb39d1497d97aa5f51833
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-15 13:49:22 +0100

    [frontend] add create action button

---
+++ frontend/src/components/data-table/data-table-view/action-buttons/create.tsx
@@ -0,0 +1,20 @@
+import { IconButton, minorScale } from 'evergreen-ui';
+import React, { StatelessComponent } from 'react';
+
+export interface CreateActionButtonProps {
+  disabled?: boolean;
+  onClick?: () => any;
+}
+
+export const CreateActionButton: StatelessComponent<
+  CreateActionButtonProps
+> = ({ disabled, onClick }) => (
+  <IconButton
+    intent="success"
+    disabled={disabled}
+    onClick={onClick}
+    icon="plus"
+    appearance="primary"
+    marginX={minorScale(1)}
+  />
+);
