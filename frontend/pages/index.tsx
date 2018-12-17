0522a0fa9686953c31fef54293d99cd324a4ba52
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-17 22:13:24 +0100

    [frontend] remove WorkerThread from index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,11 +3,6 @@ import React, { StatelessComponent } from 'react';
 
 import { Layout, LayoutProps } from 'components/layout';
 
-import {
-  AssignNextResponse,
-  WorkerThread,
-} from 'features/worker-node/worker-thread';
-
 import { AuthenticatedSidebar, Head } from 'product-specific';
 
 const renderSidebar: LayoutProps['renderSidebar'] = () => (
@@ -15,19 +10,6 @@ const renderSidebar: LayoutProps['renderSidebar'] = () => (
 );
 
 const Index: StatelessComponent = () => {
-  const assignNextResponse: AssignNextResponse = {
-    'subtask-in-progress-id': '1',
-    'subtask-id': '1',
-    'compiled-task-definition-url':
-      'http://localhost:5000/public/task-definitions/772e06b0-dad1-49eb-955f-d8726238a61a',
-    'problem-plugin-info': {
-      'assembly-name': 'FactorialTask',
-      namespace: 'FactorialTask',
-      'class-name': 'FactorialSumPlugin',
-    },
-  };
-  const distributedNodeId = '55301d87-cfc8-48c5-ad58-e188d7a39f38';
-
   return (
     <>
       <Head />
@@ -37,8 +19,6 @@ const Index: StatelessComponent = () => {
           <Card padding={80} border="default" background="tint2">
             Info here
           </Card>
-
-          <WorkerThread assignNextResponse={assignNextResponse} />
         </Pane>
       </Layout>
     </>
