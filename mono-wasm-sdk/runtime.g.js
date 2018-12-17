f80b537ca299aebd6f2cfbe755b09f4c05148d4e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-17 08:05:43 +0100

    [sdk] allow customizing deploy prefix in the runtime

--- mono-wasm-sdk/runtime.g.js
+++ mono-wasm-sdk/runtime.g.js
@@ -1,9 +1,11 @@
 
-var Module = { 
+getDeployPrefix = getDeployPrefix || function (pathSuffix) { return pathSuffix;}
+
+var Module = {
 	onRuntimeInitialized: function () {
 		MONO.mono_load_runtime_and_bcl (
 			"@VFS_PREFIX@",
-			"@DEPLOY_PREFIX@",
+			getDeployPrefix("@DEPLOY_PREFIX@"),
 			@ENABLE_DEBUGGING@,
 			[ @FILE_LIST@ ],
 			function () {
