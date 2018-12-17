822edeec13ea91e6e89cf173a081ff6a287d6912
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-12-17 13:08:23 +0100

    [server] use nullable ints as id param

--- Server/Services/Api/DistributedTaskDefinitionService.cs
+++ Server/Services/Api/DistributedTaskDefinitionService.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using JsonApiDotNetCore.Data;
 using JsonApiDotNetCore.Internal;
@@ -12,11 +9,10 @@ using Server.Models;
 namespace Server.Services.Api
 {
     /// <summary>
-    /// Customized EntityResourceService for DistributedTaskDefinitions
-    /// that ensures name uniqueness before the resources are saved
-    /// in the database.
-    ///
-    /// This is done to achieve better error reporting.
+    ///     Customized EntityResourceService for DistributedTaskDefinitions
+    ///     that ensures name uniqueness before the resources are saved
+    ///     in the database.
+    ///     This is done to achieve better error reporting.
     /// </summary>
     public class DistributedTaskDefinitionService : EntityResourceService<DistributedTaskDefinition>
     {
@@ -27,7 +23,7 @@ namespace Server.Services.Api
             IEntityRepository<DistributedTaskDefinition> repository,
             ILoggerFactory loggerFactory,
             DistributedComputingDbContext dbContext)
-        : base(jsonApiContext, repository, loggerFactory)
+            : base(jsonApiContext, repository, loggerFactory)
         {
             _dbContext = dbContext;
         }
@@ -41,23 +37,18 @@ namespace Server.Services.Api
 
         public override async Task<DistributedTaskDefinition> UpdateAsync(int id, DistributedTaskDefinition resource)
         {
-            if (resource.Name != null)
-            {
-                await EnsureUniqueName(resource.Name,id);
-            }
+            if (resource.Name != null) await EnsureUniqueName(resource.Name, id);
 
             return await base.UpdateAsync(id, resource);
         }
 
-        private async Task EnsureUniqueName(string name, int id=-1)
+        private async Task EnsureUniqueName(string name, int? id = null)
         {
             var taskDefinitionExists = await _dbContext.DistributedTaskDefinitions
-                .AnyAsync(taskDefinition => taskDefinition.Name == name && taskDefinition.Id!= id);
+                .AnyAsync(taskDefinition => taskDefinition.Name == name && (id == null || taskDefinition.Id != id));
 
             if (taskDefinitionExists)
-            {
                 throw new JsonApiException(400, $"A task definition with the name {name} already exists");
-            }
         }
     }
 }
