a07cdbe4ebc26d1d65739cef77906efe54e849af
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-12-19 22:23:50 +0100

    [frontend] update comment

--- frontend/src/features/distributed-nodes/table/table.tsx
+++ frontend/src/features/distributed-nodes/table/table.tsx
@@ -170,7 +170,7 @@ export class PureDistributedNodesTable extends Component<
   ) => {
     this.setState({
       forceFetchDataCallback: fetchData,
-      // NOTE: data is reassigned in order to re-render table
+      // NOTE: data is reassigned in order to re-render table so that forceFetchDataCallback is propagated
       data: List(this.state.data.toArray()),
     });
   };
