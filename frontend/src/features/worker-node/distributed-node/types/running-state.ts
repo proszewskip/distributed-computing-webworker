a5d469c82cf495b0e7ad089000724e7561b45432
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-19 18:03:10 +0100

    [frontend] delete unnecessary variable

--- frontend/src/features/worker-node/distributed-node/types/running-state.ts
+++ frontend/src/features/worker-node/distributed-node/types/running-state.ts
@@ -9,14 +9,12 @@ import { SubtaskWorker } from 'features/worker-node/worker';
  */
 export enum DistributedNodeRunningState {
   AskingForNewTask = 'ASKING_FOR_A_NEW_TASK',
-  StartedNewWorker = 'STARTED_NEW_WORKER_THREAD',
   WaitingForEmptyThread = 'WAITING_FOR_EMPTY_THREAD',
   WaitingForTimeout = 'WAITING_FOR_TIMEOUT',
 }
 
 export interface DistributedNodeRunningStateData {
   [DistributedNodeRunningState.AskingForNewTask]: never;
-  [DistributedNodeRunningState.StartedNewWorker]: never;
   [DistributedNodeRunningState.WaitingForEmptyThread]: never;
   [DistributedNodeRunningState.WaitingForTimeout]: {
     timeoutId: number;
@@ -35,9 +33,6 @@ export type BaseDistribuedNodeRunningStateWithData<
 export type DistributedNodeRunningStateWithData =
   | BaseDistribuedNodeRunningStateWithData<
       DistributedNodeRunningState.AskingForNewTask
-    >
-  | BaseDistribuedNodeRunningStateWithData<
-      DistributedNodeRunningState.StartedNewWorker
     >
   | BaseDistribuedNodeRunningStateWithData<
       DistributedNodeRunningState.WaitingForEmptyThread
