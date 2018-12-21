0093dad0f2fc884e4be6f71e94b9443d61ca2807
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-21 22:46:03 +0100

    [frontend] log in experiments

--- frontend/src/features/authentication/logout.ts
+++ frontend/src/features/authentication/logout.ts
@@ -3,6 +3,7 @@ import unfetch from 'isomorphic-unfetch';
 import { config } from 'product-specific';
 
 export const logout = (fetch: typeof unfetch) =>
-  fetch(`${config.serverUrl}/users/logout`, { method: 'POST' }).catch(
-    () => null,
-  );
+  fetch(`${config.serverUrl}/users/logout`, {
+    method: 'POST',
+    credentials: 'include',
+  }).catch(() => null);
