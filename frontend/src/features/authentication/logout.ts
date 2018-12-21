ffca3c666bdcb9d6977fc87f562f3e9b44438215
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-21 22:20:54 +0100

    [frontend] extract logout request to a separate function

---
+++ frontend/src/features/authentication/logout.ts
@@ -0,0 +1,8 @@
+import unfetch from 'isomorphic-unfetch';
+
+import { config } from 'product-specific';
+
+export const logout = (fetch: typeof unfetch) =>
+  fetch(`${config.serverUrl}/users/logout`, { method: 'POST' }).catch(
+    () => null,
+  );
