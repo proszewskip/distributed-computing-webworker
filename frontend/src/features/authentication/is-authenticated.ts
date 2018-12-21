36900a2b690e2d105ebd0eae062fd755f11fb660
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-21 21:43:48 +0100

    [frontend] add authentication utilities

---
+++ frontend/src/features/authentication/is-authenticated.ts
@@ -0,0 +1,13 @@
+import unfetch from 'isomorphic-unfetch';
+
+import { config } from 'product-specific';
+
+interface IsAuthenticatedResponseBody {
+  isSignedIn: boolean;
+}
+
+export async function isAuthenticated(fetch: typeof unfetch) {
+  return fetch(`${config.serverUrl}/users/is-authenticated`)
+    .then((response) => response.json())
+    .then((body: IsAuthenticatedResponseBody) => body.isSignedIn);
+}
