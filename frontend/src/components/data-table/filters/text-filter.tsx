df5c1067b417d9673536ca50a57fc265ea7f1163
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-12-22 16:09:17 +0100

    [frontend] show tooltip on focus

--- frontend/src/components/data-table/filters/text-filter.tsx
+++ frontend/src/components/data-table/filters/text-filter.tsx
@@ -1,5 +1,5 @@
 import { TextInput, Tooltip } from 'evergreen-ui';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { FilterRender } from 'react-table';
 
 type OnChange = (value: string) => any;
@@ -7,12 +7,56 @@ type OnChange = (value: string) => any;
 const onChangeFactory = (originalOnChange: OnChange) => (event: any) =>
   originalOnChange(event.target.value);
 
-export const TextFilter: FilterRender = ({ filter, onChange }) => (
-  <Tooltip content="For partial match type: 'like:value'" position="top">
-    <TextInput
-      width="100%"
-      value={filter ? filter.value : ''}
-      onChange={onChangeFactory(onChange)}
-    />
-  </Tooltip>
+export interface TextFilterProps {
+  filter: any;
+  onChange: () => any;
+}
+
+export interface TextFilterState {
+  isShown: boolean;
+}
+
+class TextFilterComponent extends PureComponent<
+  TextFilterProps,
+  TextFilterState
+> {
+  public state = {
+    isShown: false,
+  };
+
+  public render() {
+    const { filter, onChange } = this.props;
+
+    return (
+      <Tooltip
+        content="For partial match type: 'like:value'"
+        position="top"
+        isShown={this.state.isShown}
+      >
+        <TextInput
+          width="100%"
+          value={filter ? filter.value : ''}
+          onChange={onChangeFactory(onChange)}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+        />
+      </Tooltip>
+    );
+  }
+
+  private onFocus = () => {
+    this.setState({
+      isShown: true,
+    });
+  };
+
+  private onBlur = () => {
+    this.setState({
+      isShown: false,
+    });
+  };
+}
+
+export const TextFilter: FilterRender = (params) => (
+  <TextFilterComponent {...params} />
 );
