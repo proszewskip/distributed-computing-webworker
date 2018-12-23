a68b6391d30fa198fc468d97af4cdcd8abcd3314
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-23 17:52:01 +0100

    [frontend] generalize redirect function
    Allow consumers to specify a URL of their own choosing
    This way other pages can also use the same isomorphic redirect
    function to redirect to other pages, not only the login page.

--- frontend/src/product-specific/authentication/index.ts
+++ frontend/src/product-specific/authentication/index.ts
@@ -2,4 +2,3 @@ export * from './forward-cookies';
 export * from './handle-authentication-error';
 export * from './is-authenticated';
 export * from './logout';
-export * from './redirect-to-login-page';
--- frontend/src/product-specific/config.ts
+++ frontend/src/product-specific/config.ts
@@ -3,4 +3,6 @@ export const config = {
    * `serverUrl` is safe to use only client-side as it does not contain the serven hostname
    */
   serverUrl: '/api',
+
+  loginPageUrl: '/authentication/login',
 };
