ef27c8f3f6f1f233f07431ba5f9d65b425bc99b8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-23 12:09:47 +0100

    [frontend] inject more properties into AppContext

--- frontend/src/product-specific/app-context/types.ts
+++ frontend/src/product-specific/app-context/types.ts
@@ -1,3 +1,4 @@
+import unfetch from 'isomorphic-unfetch';
 import { NextComponentType, NextContext } from 'next';
 import { DefaultQuery } from 'next/router';
 
@@ -12,6 +13,19 @@ export interface AppContext<Q extends DefaultQuery = DefaultQuery>
    * Contains augmented headers and baseURL so that authentication also works server-side
    */
   kitsuFactory: KitsuFactory;
+
+  /**
+   * Server URL that is safe to use server-side and client-side
+   */
+  serverUrl: string;
+
+  /**
+   * Safe to use server-side and client-side. Injects headers and server URL so that authentication
+   * works.
+   */
+  fetch: typeof unfetch;
+
+  redirectToLoginPage: () => void;
 }
 
 export type AppPageComponentType<
