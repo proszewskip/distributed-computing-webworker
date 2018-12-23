bb56ecfe418d35930c2fd59873d3cc7b2758fbb3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-23 11:37:51 +0100

    [frontend] move authentication to product-specific directory

--- frontend/src/product-specific/authentication/handle-authentication-error.ts
+++ frontend/src/product-specific/authentication/handle-authentication-error.ts
@@ -1,14 +1,19 @@
 import { isJsonApiErrorResponse } from 'error-handling';
 
+export type HandleAuthenticationErrorFn<Props> = (
+  error: unknown,
+) => Promise<Props>;
+
 /**
  * Handles JSON API errors that resulted from the user being unauthenticated.
  *
+ * Treat this function as middleware, as it **rethrows the original error**.
+ *
  * @param redirect Function to be called if the user should be redirected
- * @param fallbackProps Fallback props that will be used
  */
 export const handleAuthenticationErrorFactory = <Props>(
   redirect: () => void,
-) => (error: unknown) => {
+): HandleAuthenticationErrorFn<Props> => (error) => {
   if (isJsonApiErrorResponse(error)) {
     const isForbidden = error.errors.find(
       (singleError) => singleError.status === '403',
@@ -23,5 +28,5 @@ export const handleAuthenticationErrorFactory = <Props>(
    * TODO: handle `fetch` responses
    */
 
-  return Promise.reject<Props>(error);
+  return Promise.reject(error);
 };
