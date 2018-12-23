ea10fd9b26cb2fb2c428349c985a5f004def8fb2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-23 12:45:20 +0100

    [frontend] fix logout

--- frontend/src/product-specific/authentication/logout.ts
+++ frontend/src/product-specific/authentication/logout.ts
@@ -1,8 +1,6 @@
 import unfetch from 'isomorphic-unfetch';
 
-import { config } from 'product-specific';
-
 export const logout = (fetch: typeof unfetch) =>
-  fetch(`${config.serverUrl}/users/logout`, {
+  fetch('/users/logout', {
     method: 'POST',
   }).catch(() => null);
