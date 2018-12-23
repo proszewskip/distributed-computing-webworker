2c8c80bad69b8a225be36db080c9b30fba2bb69f
proszewskip <przemek.proszewski@gmail.com>
2018-12-24 00:50:56 +0100

    [frontend] update props, clear timeout

--- frontend/src/features/distributed-nodes/table/date-cell.tsx
+++ frontend/src/features/distributed-nodes/table/date-cell.tsx
@@ -1,13 +1,14 @@
 import { Tooltip } from 'evergreen-ui';
 import React from 'react';
 
-import { getRerenderTimeout, RelativeTime } from 'components/relative-time';
+import { RelativeTime } from 'components/relative-time';
 
-export const DateCell = (row: { value: any }) => (
-  <Tooltip content={new Date(row.value).toLocaleString()}>
-    <RelativeTime
-      dateString={row.value}
-      getRerenderTimeout={getRerenderTimeout}
-    />
-  </Tooltip>
-);
+export const DateCell = (row: { value: any }) => {
+  const eventDate = new Date(row.value);
+
+  return (
+    <Tooltip content={eventDate.toLocaleString()}>
+      <RelativeTime eventDate={eventDate} />
+    </Tooltip>
+  );
+};
