649157b8b7361b6d617da7c720348c9fa7d77858
proszewskip <przemek.proszewski@gmail.com>
2018-12-24 15:38:50 +0100

    [frontend] move tooltip to the left side of the cell

--- frontend/src/components/data-table/cells/long-text-cell.tsx
+++ frontend/src/components/data-table/cells/long-text-cell.tsx
@@ -2,7 +2,7 @@ import { Text, Tooltip } from 'evergreen-ui';
 import React from 'react';
 
 export const LongTextCell = (row: { value: any }) => (
-  <Tooltip content={row.value} position="bottom-left">
+  <Tooltip content={row.value} position="left">
     <Text>{row.value}</Text>
   </Tooltip>
 );
