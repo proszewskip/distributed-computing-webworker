1b98ae341372ed36f40e9136f0685c987fc91302
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-12-27 18:23:50 +0100

    [frontend] show form errors with details

--- frontend/src/components/form/errors/error-alert.tsx
+++ frontend/src/components/form/errors/error-alert.tsx
@@ -1,5 +1,6 @@
-import { Alert, Paragraph } from 'evergreen-ui';
+import { Alert, Pane } from 'evergreen-ui';
 import { FormikErrors, FormikTouched, FormikValues } from 'formik';
+import { Dictionary } from 'ramda';
 import React, { StatelessComponent } from 'react';
 
 interface ErrorAlertProps<V = any> {
@@ -13,22 +14,28 @@ export const ErrorAlert: StatelessComponent<ErrorAlertProps> = (
 ) => {
   const errorsAlertVisible = hasFormErrors(errorAlertProps);
 
-  const errorsFromServer = getErrorsFromServer(errorAlertProps).map(
-    (errorMessage) => (
-      <Paragraph key={errorMessage}> {errorMessage} </Paragraph>
-    ),
-  );
+  const errorsFromServer = getServerAlerts(errorAlertProps);
 
   return (
     <>
       {errorsAlertVisible && (
         <Alert intent="danger" title="Form contains errors." />
       )}
-      {errorsFromServer.length > 0 && (
-        <Alert intent="danger" children={errorsFromServer} />
-      )}
+      {errorsFromServer.length > 0 && <Pane>{errorsFromServer}</Pane>}
     </>
   );
+};
+
+const getServerAlerts = (errorAlertProps: ErrorAlertProps) => {
+  const errorsFromServer = getErrorsFromServer(errorAlertProps);
+
+  const alertsList = Object.keys(errorsFromServer).map((title, index) => (
+    <Alert key={index} title={title} intent="danger">
+      {errorsFromServer[title]}
+    </Alert>
+  ));
+
+  return alertsList;
 };
 
 const hasFormErrors = ({ touched, errors, values }: ErrorAlertProps) => {
@@ -43,8 +50,13 @@ const getErrorsFromServer = ({ errors, values }: ErrorAlertProps) => {
   const fieldNames = Object.keys(values);
   const errorKeys = Object.keys(errors);
 
-  const serverErrors = errorKeys.filter((key) => !fieldNames.includes(key));
-  // TODO: use values after custom server errors are sent as `Detail`.
+  const serverErrorsKeys = errorKeys.filter((key) => !fieldNames.includes(key));
+
+  const serverErrors: Dictionary<string | undefined> = {};
+
+  for (const key of serverErrorsKeys) {
+    serverErrors[key] = errors[key] as any;
+  }
 
   return serverErrors;
 };
