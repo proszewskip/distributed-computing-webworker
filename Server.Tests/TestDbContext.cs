a4736ffe94bc9e863c5f0def5827a6b5b8228816
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-12-27 14:32:42 +0100

    [server][tests] add mock DbContext utilities

---
+++ Server.Tests/TestDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Server.Models;
+
+namespace Server.Tests
+{
+    public class TestDbContext : DistributedComputingDbContext
+    {
+        public TestDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.ConfigureWarnings(warningsConfigurationBuilder =>
+            {
+                warningsConfigurationBuilder.Ignore(InMemoryEventId.TransactionIgnoredWarning);
+            });
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Ignore string[] properties as those are not supported when using InMemoryDatabase
+            modelBuilder.Entity<DistributedTask>().Ignore(distributedTask => distributedTask.Errors);
+            modelBuilder.Entity<SubtaskInProgress>().Ignore(subtaskInProgress => subtaskInProgress.Errors);
+        }
+    }
+}
