c7526ebc72e676d95df971e66b9ff309722f03cd
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-01 22:48:58 +0100

    [frontend] update config and serverUrlProvider based on new configuration

--- frontend/src/product-specific/config.ts
+++ frontend/src/product-specific/config.ts
@@ -1,8 +1,14 @@
 export const config = {
   /**
-   * `serverUrl` is safe to use only client-side as it does not contain the serven hostname
+   * `serverUrl` is safe to use only client-side as it does not contain the server hostname
+   * Use `serverUrlProvider` for an isomorphic URL (safe to use in both environments)
    */
   serverUrl: '/api',
+
+  /**
+   * Server URL when the app runs in production (as a Docker container)
+   */
+  productionServerUrl: 'http://backend:5000',
 
   loginPageUrl: '/authentication/login',
 };
