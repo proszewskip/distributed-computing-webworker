eddbff33b42fc74e2c298117834cadd20d7a3725
proszewskip <proszewskip@student.mini.pw.edu.pl>
2019-01-02 10:26:59 +0100

    [frontend] show dialog with subtask errors in table

--- frontend/src/features/subtasks/table/subtask-status-cell.tsx
+++ frontend/src/features/subtasks/table/subtask-status-cell.tsx
@@ -1,15 +1,92 @@
-import { Alert, Text } from 'evergreen-ui';
-import React, { ReactNode } from 'react';
+import { Alert, Button, Dialog, Pane, Text } from 'evergreen-ui';
+import React, { Component, ReactNode } from 'react';
 
-import { SubtaskStatus } from 'models';
+import { Subtask, SubtaskStatus } from 'models';
 
-const statusMapping: { [status in SubtaskStatus]: ReactNode } = {
-  [SubtaskStatus.Done]: <Text>Done</Text>,
-  [SubtaskStatus.Executing]: <Text>In progress</Text>,
-  [SubtaskStatus.Error]: <Alert intent="danger">Error</Alert>,
-  [SubtaskStatus.WaitingForExecution]: <Text>Waiting for execution</Text>,
+import { preventPropagationHandler } from 'utils/table/prevent-propagation-handler';
+
+export const SubtaskStatusCell = (row: { value: Subtask }) => {
+  return <SubtaskStatusCellContent subtask={row.value} />;
 };
 
-export const SubtaskStatusCell = (row: { value: SubtaskStatus }) => {
-  return statusMapping[row.value];
-};
+interface ErrorsDialogState {
+  isShown: boolean;
+}
+
+interface ErrorsDialogProps {
+  subtask: Subtask;
+}
+
+class SubtaskStatusCellContent extends Component<
+  ErrorsDialogProps,
+  ErrorsDialogState
+> {
+  public state: ErrorsDialogState = {
+    isShown: false,
+  };
+
+  public render() {
+    return (
+      <>
+        {this.renderStatus()}
+        {this.renderDialog()}
+      </>
+    );
+  }
+
+  private getStatusMapping = (subtaskStatus: SubtaskStatus): ReactNode => {
+    switch (subtaskStatus) {
+      case SubtaskStatus.Done:
+        return <Text>Done</Text>;
+      case SubtaskStatus.Executing:
+        return <Text>In progress</Text>;
+      case SubtaskStatus.Error:
+        return (
+          <Button intent="danger" onClick={this.onStatusClick}>
+            Error
+          </Button>
+        );
+      case SubtaskStatus.WaitingForExecution:
+        return <Text>Waiting for execution</Text>;
+    }
+  };
+
+  private renderStatus = () => {
+    const { subtask } = this.props;
+
+    return <Pane>{this.getStatusMapping(subtask['subtask-status'])}</Pane>;
+  };
+
+  private renderDialog = () => {
+    const { isShown } = this.state;
+    const { subtask } = this.props;
+
+    return (
+      <Dialog
+        isShown={isShown}
+        title="Subtask errors"
+        onCloseComplete={this.onCloseComplete}
+        hasFooter={false}
+        white-space="normal"
+      >
+        <Pane>
+          {subtask.errors.map((error, index) => (
+            <Alert key={index} intent="danger">
+              <Text>{error}</Text>
+            </Alert>
+          ))}
+        </Pane>
+      </Dialog>
+    );
+  };
+
+  private onCloseComplete = () => {
+    this.setState({ isShown: false });
+  };
+
+  private onStatusClick = (event: any) => {
+    preventPropagationHandler(event);
+
+    this.setState({ isShown: true });
+  };
+}
