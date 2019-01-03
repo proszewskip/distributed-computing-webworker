e83a04720595a7650af9f2d7ee0b28faf01f1ed1
proszewskip <przemek.proszewski@gmail.com>
2019-01-03 11:30:23 +0100

    [task] add polynomial minimum problem

---
+++ example-problems/PolynomialMinimumProblem/PolynomialMinimumProblem/Models/TaskInput.cs
@@ -0,0 +1,10 @@
+namespace PolynomialMinimumProblem
+{
+    public class TaskInput
+    {
+        public double LowerBound { get; set; }
+        public double UpperBound { get; set; }
+        public int SubtasksCount { get; set; }
+        public double[] Coefficients { get; set; }
+    }
+}
