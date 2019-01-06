f8cbdd61b74acaf3832f0b786de31512180e07d1
proszewskip <proszewskip@student.mini.pw.edu.pl>
2019-01-06 14:55:46 +0100

    [general] store containers id in a variable

--- backup.sh
+++ backup.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+backendContainter=$(docker ps | grep backend | cut -d' ' -f1)
+postgresContainer=$(docker ps | grep postgres | cut -d' ' -f1)
 
 #DB backup
-docker exec -t $(docker ps | grep postgres | cut -d' ' -f1) pg_dumpall -c -U postgres > db_backup.sql
+docker exec -t $postgresContainer pg_dumpall -c -U postgres > db_backup.sql
 
 #Task definitions backup
-docker exec -t $(docker ps | grep backend | cut -d' ' -f1) tar -zcvf task-definitions.tar.gz /task-definitions/ /compiled-task-definitions/;
-docker cp $(docker ps | grep backend | cut -d' ' -f1):/app/backup.tar.gz task-definitions.tar.gz
+docker exec -t $backendContainter tar -zcvf task-definitions.tar.gz /task-definitions/ /compiled-task-definitions/;
+docker cp $backendContainter:/app/task-definitions.tar.gz task-definitions.tar.gz
 
 #Compress both backups
 tar -zcvf backup_`date +%d-%m-%Y"_"%H_%M_%S`.tar.gz task-definitions.tar.gz db_backup.sql
 
 #Cleanup 
 rm db_backup.sql task-definitions.tar.gz
-docker exec -t $(docker ps | grep backend | cut -d' ' -f1) rm task-definitions.tar.gz
-
+docker exec -t $backendContainter rm task-definitions.tar.gz
