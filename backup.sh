b629ea68551389989d26b89e111462c0615a1b3a
proszewskip <proszewskip@student.mini.pw.edu.pl>
2019-01-06 13:57:40 +0100

    [general] add backup script

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker exec -t $(docker ps | grep postgres | cut -d' ' -f1) pg_dumpall -c -U postgres > db_backup.sql
+docker exec -t $(docker ps | grep backend | cut -d' ' -f1) tar -zcvf task-definitions.tar.gz /task-definitions/ /compiled-task-definitions/;
+docker cp $(docker ps | grep backend | cut -d' ' -f1):/app/backup.tar.gz task-definitions.tar.gz
+tar -zcvf backup_`date +%d-%m-%Y"_"%H_%M_%S`.tar.gz task-definitions.tar.gz db_backup.sql
+rm db_backup.sql task-definitions.tar.gz
+docker exec -t $(docker ps | grep backend | cut -d' ' -f1) rm task-definitions.tar.gz
+
