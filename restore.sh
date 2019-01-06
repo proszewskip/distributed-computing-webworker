349ca8faab59f5a50686488968fb6402aedb40a5
proszewskip <przemek.proszewski@gmail.com>
2019-01-06 14:55:46 +0100

    [general] store containers id in a variable

--- restore.sh
+++ restore.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+ 
+backendContainer=$(docker ps | grep backend | cut -d' ' -f1)
+postgresContainer=$(docker ps | grep postgres | cut -d' ' -f1)
 
 #Unpack archive
 mkdir backup
 tar -xzf $1 -C backup
 
 #Restore DB
-cat backup/db_backup.sql | docker exec -i $(docker ps | grep postgres | cut -d' ' -f1) psql -U postgres
+cat backup/db_backup.sql | docker exec -i $postgresContainer psql -U postgres
 
 #Restore task definitions
-docker cp backup/task-definitions.tar.gz $(docker ps | grep backend | cut -d' ' -f1):/
-docker exec -t $(docker ps | grep backend | cut -d' ' -f1) tar -xzf /task-definitions.tar.gz -C /
-docker exec -t $(docker ps | grep backend | cut -d' ' -f1) rm /task-definitions.tar.gz
+docker cp backup/task-definitions.tar.gz $backendContainer:/
+docker exec -t $backendContainer tar -xzf /task-definitions.tar.gz -C /
+docker exec -t $backendContainer rm /task-definitions.tar.gz
 
 #Cleanup
 rm backup -rf
