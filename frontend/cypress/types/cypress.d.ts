f6cf2b5666faaa3677332ac86b89691d2ca915db
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-12 19:43:36 +0100

    [frontend] add uploadFile cypress command

--- frontend/cypress/types/cypress.d.ts
+++ frontend/cypress/types/cypress.d.ts
@@ -1,12 +1,27 @@
 /// <reference types="cypress" />
 
 declare namespace Cypress {
-  interface Chainable {
+  interface Chainable<Subject> {
     /**
      * Sends a login request to the API using the default username and password
      *
-     * If the `password` parameter is provided, it is used as an override for the password
+     * @param password If provided, overrides the used password
      */
     login(password?: string): void;
+
+    /**
+     * Selects a file for upload
+     *
+     * @see https://github.com/cypress-io/cypress/issues/170#issuecomment-404931741
+     *
+     * @param fileName Name of the file in the `fixtures` directory
+     * @param selector Selector for a input[type=file]
+     * @param mimeType Mime type of the file. `application/octet-stream` by default
+     */
+    uploadFile(
+      fileName: string,
+      selector: string,
+      mimeType?: string,
+    ): Chainable<Subject>;
   }
 }
