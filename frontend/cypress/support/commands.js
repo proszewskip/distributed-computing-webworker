6db3be38c1b12e569c6333d81911224bdfbc5029
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-12 18:31:47 +0100

    [frontend] install cypress.io, add login command, add Typescript types

---
+++ frontend/cypress/support/commands.js
@@ -0,0 +1,36 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (password = 'D1stributed$') => {
+  cy.log('Sending a login request straight to the API.');
+  cy.log('If it fails, make sure the backend is running');
+  cy.log('If it is, restart the backend to restart the database');
+
+  cy.request('POST', 'api/users/login', {
+    username: 'admin',
+    password,
+  });
+});
